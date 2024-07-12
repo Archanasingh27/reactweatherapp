1a72b3b22fefd0b8f5f6dc326b7428586f06c827
Archanasingh27 <rathourarchna922@gmail.com>
2024-07-12 11:07:56 +0530

    my first commit react-app

---
+++ src/api.js
@@ -0,0 +1,14 @@
+
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'e33d26b05bmsha9e50340e597e6dp12e8a6jsn9fc5f6d6a2c9',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "c4fccabd443e0ed633b2c253c2f0d7ad";
